72fef21929d5e902622aa989d799f43acb6b3ccd
Vlad Tudor <vladtudor55@yahoo.com>
2019-11-14 17:38:13 +0100

    Insert bookmarks (part 2)

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -3,48 +3,140 @@
  * See LICENSE in the project root for license information.
  */
 
+// Office.onReady(info => {
+//   if (info.host === Office.HostType.Word) {
+//     //document.getElementById("sideload-msg").style.display = "none";
+//     document.getElementById("app-body").style.display = "flex";
+//   }
+// });
+
+//  // The initialize function must be run each time a new page is loaded
+//  Office.initialize = function (reason) {
+//   $(document).ready(function(){
+//     _doc = Office.context.document;
+//     tryUpdatingSelectedWord();
+//     _doc.addHandlerAsync("documentSelectionChanged", tryUpdatingSelectedWord);
+//     $('#getOOXML').click(function() { getOOXML(); });
+//     $('#setOOXML').click(function () { setOOXML(); });
+
+//     loadSampleData();
+//   });
+// };
+
+// function getOOXML() {
+//   var report = document.getElementById("status");
+
+//   while (report.hasChildNodes()) {
+//       report.removeChild(report.lastChild);
+//   }
+
+//   Office.context.document.getSelectedDataAsync(
+//       Office.CoercionType.Ooxml,
+//       { valueFormat: Office.ValueFormat.Formatted, filterType: Office.FilterType.All },
+//       function (result) {
+//           var textArea = document.getElementById("dataOOXML");
+//           if (result.status == "succeeded") {
+//               currentOOXML = result.value;
+              
+
+//               while (textArea.hasChildNodes()) {
+//                   textArea.removeChild(textArea.lastChild);
+//               }
+//               textArea.appendChild(document.createTextNode(currentOOXML));
+
+//               // Tell the user we succeeded
+//               report.innerText = "Got It --- Success!!";
+//           }
+//           else {
+//               // This runs if the getSnelectedDataAsync method does not return a success flag
+//               currentOOXML = "";
+//               report.innerText = result.error.message;
+//           }
+//       });
+// }
+
+// function setOOXML() {
+//   var report = document.getElementById("status");
+//   while (report.hasChildNodes()) {
+//       report.removeChild(report.lastChild);
+//   }
+
+//   if (currentOOXML != "") {
+
+
+//       Office.context.document.setSelectedDataAsync(
+//           currentOOXML,
+//           { coercionType: Office.CoercionType.Ooxml },
+//           function (result) {
+//               console.log(result)
+//               // Update the report element
+//               if (result.status == "succeeded") {
+//                   report.innerText = "Set It --- Success!!";
+//               }
+//               else {
+//                   report.innerText = result.error.message;
+//                   while (textArea.hasChildNodes()) {
+//                       textArea.removeChild(textArea.lastChild);
+//                   }
+//               }
+//           });
+//   }
+//   else {
+
+//       report.innerText = "There is currently no OOXML to insert!"
+//           + " Please select some of your document and click [Get OOXML] first!";
+//   }
+// }
+
  var _doc;
  var lastLookup; 
- var sFileName;
- var sOoxml;
+ var currentOOXML = "";
 
-Office.onReady(info => {
-  if (info.host === Office.HostType.Word) {
-    document.getElementById("sideload-msg").style.display = "none";
-    document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
-  }
-});
+(function () {
+  "use strict";
 
-export async function run() {
-  return Word.run(async context => {
-    /**
-     * Insert your Word code here
-     */
+  // The initialize function must be run each time a new page is loaded.
+  Office.initialize = function (reason) {
+      $(document).ready(function () {
+        _doc = Office.context.document;
+          tryUpdatingSelectedWord();
+          _doc.addHandlerAsync("documentSelectionChanged", tryUpdatingSelectedWord);
 
-    // insert a paragraph at the end of the document.
-    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
+          $("#template-description").text("This sample creates a bookmark at the caret position or around a selection.  Note, there are bugs in Word that prevent anchor bookmarks at the caret position.  To get around those bugs I insert '[delete - necessary to avoid bug in Word 2016 (16.0.6925.1018)]' text after an anchor bookmark.  I'm trying to submit this to Microsoft officially as a bug, along with a suggestion to support bookmarks natively just like they support content controls.  Note, Word can create these and the OPC, OOXML, WordProcessingML is the same so the bug is in the '.insertOoxml(...)' methods.");
+          $('#button-text').text("Bookmark!");
+          $('#button-desc').text("Places a bookmark at the caret or around the selection");
 
-    // change the paragraph color to blue.
-    paragraph.font.color = "blue";
+          // loadSampleData();
 
-    await context.sync();
-  });
-}
+          // Add a click event handler for the bookmark button.
+          $('#bookmark-button').click(
+              placeBookmark);
+      });
+  };
 
- // The initialize function must be run each time a new page is loaded
- Office.initialize = function (reason) {
-  $(document).ready(function(){
-    _doc = Office.context.document;
-    tryUpdatingSelectedWord();
-    _doc.addHandlerAsync("documentSelectionChanged", tryUpdatingSelectedWord);
-    $('#insertContentControls').click(insertContentControls);
-    $('#refBookmark').click(refBookmark);
-    $('#bookmarkSelection').click(bookmarkSelection);
-  });
-};
+  // This function inserts sample data, or a pre-written piece of text. Don't use outside of development environment
 
-function tryUpdatingSelectedWord() {
+  // function loadSampleData() {
+
+  //     // Run a batch operation against the Word object model.
+  //     Word.run(function (context) {
+
+  //         // Create a proxy object for the document body.
+  //         var body = context.document.body;
+
+  //         // Queue a commmand to clear the contents of the body.
+  //         body.clear();
+  //         // Queue a command to insert text into the end of the Word document body.
+  //         body.insertText("This is a sample text inserted in the document",
+  //                         Word.InsertLocation.end);
+
+  //         // Synchronize the document state by executing the queued commands, and return a promise to indicate task completion.
+  //         return context.sync();
+  //     })
+  //     .catch(errorHandler);
+  // }
+
+  function tryUpdatingSelectedWord() {
   _doc.getSelectedDataAsync(Office.CoercionType.Text, selectedTextCallback); 
 }
 
@@ -58,170 +150,59 @@ function selectedTextCallback(selectedText) {
   }
 }
 
- // Bookmark the current selection
- function bookmarkSelection() {
-  //Get the current selection in order to bookmark
-  Office.context.document.getSelectedDataAsync(Office.CoercionType.Text, 
-      function (resultGet) {
-          if (resultGet.status === Office.AsyncResultStatus.Succeeded) {
-              selection = resultGet.value;
-              //Get the WordOpenXML for creating a bookmark
-              $.ajax({
-                  type: "GET",
-                  url: sFileName,
-                  dataType: "xml",
-                  success: function (resultOoxml) {
-                    Ooxml = resultOoxml;
-                    var textNode = 
-                        Ooxml.documentElement.getElementsByTagNameNS('*', 't');
-                    //Replace the text to be bookmarked with the selection
-                    textNode[0].textContent = selection;
-                    sOoxml = 
-                      new XMLSerializer().serializeToString(Ooxml.documentElement);
-                    console.log(sOoxml);
-                    //Write the bookmark back to the document
-                    Office.context.document.setSelectedDataAsync(sOoxml, 
-                       { coercionType: Office.CoercionType.Ooxml },
-                        function (result) {
-                          if (result.status === "succeeded") {
-                              app.showNotification('Success!!');
-                          } else {
-                              app.showNotification('Error:', result.error.message);
-                          }
-                        });
-                    }
-              });
-          }
-          else {
-            app.showNotification('Error:', resultGet.error.message);
-          }
+  function placeBookmark() {
+
+      Word.run(function (context) {
+
+          // Create a bookmark ID - 0 works just fine as Word will generate a new number upon insert which is awesome!
+          var bkmkId = 0;
+
+          // Create a bookmark Name - it must be unique or it will overwrite a current one!
+          // It must be no longer than 40 characters!
+          // Please see the behavior here:
+          var bkmkName = "_TOC_MANUAL_" + new Date().getTime();
+
+          // Queue a command to get the current selection and then
+          // create a proxy range object with the results.
+          var range = context.document.getSelection();
+
+          // Use the extension to insert the bookmark.  It really should be this easy.  See the header of the
+          // extension for an idea of the current suggested API for Microsoft.  I only wrote a quick insertBookmark
+          // method that doesn't quite fit the signature for what's proposed btw.  I wanted this to be quick and
+          // minimal.  Plus, the extension should really extend range, document.body, etc.
+          officeWordExtension.insertBookmark(bkmkId, bkmkName, handleSuccess, errorHandler);
       });
-}
-
-    //   //Insert a reference to the bookmark at the current selection
-    // function refBookmark() {
-
-    //   $.ajax({
-    //       type: "GET",
-    //       url: sFileName,
-    //       dataType: "xml",
-    //       success: function (resultOoxml) {
-    //         Ooxml = resultOoxml;
-    //         var textNode = Ooxml.documentElement.getElementsByTagNameNS('*', 't');
-    //         //Replace the selection with the Ref field.
-    //         //Since it's not possible to "look up" the bookmark using the
-    //         //2013 APIs the text content of the bookmark is not known - 
-    //         //the user will have to update the field manually.
-    //         textNode[0].textContent = "Press F9 to update the field";
-    //         sOoxml = new XMLSerializer().serializeToString(Ooxml.documentElement);
-    //         console.log(sOoxml);
-    //         //Write the Ref field back to the document
-    //         Office.context.document.setSelectedDataAsync(sOoxml, 
-    //             { coercionType: Office.CoercionType.Ooxml },
-    //             function (result) {
-    //                 if (result.status === Office.AsyncResultStatus.Succeeded) {
-    //                     app.showNotification('Success!!');
-    //                 } else {
-    //                     app.showNotification('Error:', result.error.message);
-    //                 }
-    //             });
-    //       }
-    //   });
-    // }
-
-     async function insertContentControls() {
-      await Word.run(async (context) => {
-          // Create a proxy object for the document body.
-
-          let range = context.document.getSelection();
-          range.load()
-          context.trackedObjects.add(range);
-          // Queue a commmand to get the OOXML contents of the body.
-          // var bodyOOXML = body.getOoxml();
-
-          // Synchronize the document state by executing the queued commands,
-          // and return a promise to indicate task completion.
-          return context.sync(range)
-      }).then(function (range) {
-          range.insertOoxml(
-              '<pkg:package xmlns:pkg="http://schemas.microsoft.com/office/2006/xmlPackage">' +
-              '<pkg:part pkg:name="/_rels/.rels" pkg:contentType="application/vnd.openxmlformats-package.relationships+xml" pkg:padding="512" >' +
-              '<pkg:xmlData ><Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships" >' +
-              '<Relationship Id="rId1" Type = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument" Target="word/document.xml" /></Relationships>' +
-              '</pkg:xmlData ></pkg:part><pkg:part pkg:name="/word/_rels/document.xml.rels" pkg:contentType="application/vnd.openxmlformats-package.relationships+xml" pkg:padding="256">' +
-              '<pkg:xmlData><Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">' +
-              '<Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/comments" Target="comments.xml" xmlns="http://schemas.openxmlformats.org/package/2006/relationships" />' +
-              '</Relationships></pkg:xmlData></pkg:part><pkg:part pkg:name="/word/document.xml" pkg:contentType="application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml"><pkg:xmlData>' +
-              '<w:document xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" xmlns:w15="http://schemas.microsoft.com/office/word/2012/wordml">' +
-              '<w:body><w:p><w:commentRangeStart w:id="0"/><w:r>' +
-              '          <w:sdt>' +
-              '              <w:sdtPr>' +
-              '                <w:alias w:val="MyContentControlTitle"/>' +
-              '                <w:id w:val="1382295294"/>' +
-              '                <w15:appearance w15:val="hidden"/>' +
-              '              </w:sdtPr>' +
-              '              <w:sdtContent>' +
-              '                  <w:r>' +
-              '                  <w:t>' + range.text + '</w:t>' +
-              '                </w:r>' +
-              '              </w:sdtContent>' +
-              '            </w:sdt>' +
-              '</w:r><w:commentRangeEnd w:id="0"/>' +
-              '<w:commentReference w:id="0"/></w:p></w:body></w:document></pkg:xmlData></pkg:part>' +
-              '<pkg:part xmlns:pkg="http://schemas.microsoft.com/office/2006/xmlPackage" pkg:name="/word/comments.xml" pkg:contentType="application/vnd.openxmlformats-officedocument.wordprocessingml.comments+xml">' +
-              '<pkg:xmlData><w:comments xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main">' +
-              '<w:comment xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" w:id="0">' +
-              '<w:p>' +
-              '<w:r>' +
-              '<w:t>Comment</w:t>' +
-              '</w:r>' +
-              '<w:hyperlink Id="rId2" w:history="1">' +
-              '<w:r>' +
-              '<w:rPr>' +
-              '<w:rStyle w:val="Hyperlink"/>' +
-              '</w:rPr>' +
-              '<w:t>Google</w:t>' +
-              '</w:r>' +
-              '</w:hyperlink>' +
-              '</w:p>' +
-              '</w:comment></w:comments>' +
-              '</pkg:xmlData></pkg:part><pkg:part pkg:name="/word/_rels/comments.xml.rels" pkg:contentType="application/vnd.openxmlformats-package.relationships+xml">' +
-              '<pkg:xmlData><Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"></Relationships></pkg:xmlData></pkg:part></pkg:package>',
-              "Replace"
-          );
-
-          // myCC.appearance = "Tags"; // you can also set it to 'boundingBox' or  'tags'
-          // myCC.title = "BindingID"
-          // console.log(myCC)
-
-           //Office.context.addHandlerAsync(myCC.onDataChanged, changeMe())
-           //document.addHandlerAsync(myCC.onSelectionChanged, touchMe())
-
-
-          function touchMe() {
-              console.log("Don't touch me!")
-          }
-
-          function ChangeMe(eventData) {
-              console.log("Eeew")
-              console.log(eventData)
-          }
-          return range.context.sync();
-      }).then(function (range) {
-          Office.context.document.bindings.addFromNamedItemAsync("MyContentControlTitle", "text", {}, function (result) {
-              console.log(result.status);
-              if (result.status == "succeeded") {
-                  // lets create an event!
-                  result.value.addHandlerAsync(Office.EventType.BindingSelectionChanged, function () {
-                      console.log("Are you looking at me?");
-                  })
-                  result.value.addHandlerAsync(Office.EventType.BindingDataChanged, function (evt) {
-                      console.log("Don't touch me you pervert!");
-                      console.log(evt)
-                  })
-              }
-          });
-          range.context.trackedObjects.remove(range);
-          return context.sync();
-      })
   }
+
+  function handleSuccess() {
+      showNotification("Success:", "It worked, use Word to check for your bookmark!");
+  }
+
+  //$$(Helper function for treating errors, $loc_script_taskpane_home_js_comment34$)$$
+  function errorHandler(error) {
+      // $$(Always be sure to catch any accumulated errors that bubble up from the Word.run execution., $loc_script_taskpane_home_js_comment35$)$$
+      showNotification("Error:", error);
+      console.log("Error: " + error);
+      if (error instanceof OfficeExtension.Error) {
+          console.log("Debug info: " + JSON.stringify(error.debugInfo));
+      }
+  }
+
+  // Helper function for displaying notifications
+  function showNotification(header, content) {
+
+      var report = document.getElementById("status");
+       while (report.hasChildNodes()) {
+      report.removeChild(report.lastChild);
+       }
+
+      $("#notificationHeader").text(header);
+      $("#notificationBody").text(content);
+      report.innerText = "Congrats, your bookmark has been inserted!" 
+//           + " Please select some of your document and click [Get OOXML] first!";
+      // messageBanner.showBanner();
+      // messageBanner.toggleExpansion();
+  }
+})();
+
+
